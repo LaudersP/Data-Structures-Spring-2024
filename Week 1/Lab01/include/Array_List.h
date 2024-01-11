21d3afded1769eb995a95a68de84ba4b2f63e6bb
Parker Lauders <parkerlauders04@gmail.com>
2024-01-10 20:16:06 -0500

    Added functions needed for insert, clear, get and find

--- Week 1/Lab01/include/Array_List.h
+++ Week 1/Lab01/include/Array_List.h
@@ -4,16 +4,80 @@
 #include <string>
 
 namespace ssuds {
+	/**
+	* @brief ArrayList class for dynamic array of strings.
+	*
+	* This class provides a basic functionality for managing an array of strings with dynamic memory allocation.
+	*/
 	class ArrayList {
 	protected:
-		std::string* _data;
-		unsigned int _size;
-	public:
-		ArrayList();
-		~ArrayList();
-		void Append(std::string s);
-		unsigned int Size();
+		std::string* _data; ///< Pointer to the array of strings.
+		unsigned int _size; ///< Size of array.
 
+	public:
+		/**
+		* @brief Constructor of ArrayList class.
+		* 
+		* Initializes default values for ArrayList.
+		*/
+		ArrayList();
+
+		/**
+		* @brief Destructor of ArrayList class.
+		*
+		* Deallocates memory used for ArrayList.
+		*/
+		~ArrayList();
+
+		/**
+		* @brief Append a string to the end of the ArrayList.
+		* 
+		* @param s The string to be appended.
+		*/
+		void Append(std::string s);
+
+		/**
+		* @brief Gets the current length of the array list.
+		* 
+		* @return The current lenght of the list.
+		*/
+		unsigned int Size() const;
+
+		/**
+		* @breif Insert a string at a set position in the list.
+		* 
+		* @param s The string to be inserted.
+		* @param position The index for the string to be inserted at.
+		*/
+		void Insert(std::string s, const unsigned int position);
+
+		/**
+		* @breif Clears the list and resets it back to the beginning values.
+		* 
+		* Similar to the destructor but meant to be used during list's lifetime.
+		*/
+		void Clear();
+
+		/**
+		* @breif Gets a string at a defined index.
+		* 
+		* @param index The position of the desired string.
+		* 
+		* @return The string at position of the index value.
+		*/
+		std::string Get(const unsigned int index) const;
+
+		/**
+		* @breif Find a string in the list, with a optional starting location.
+		* 
+		* @param s String to be found.
+		* @param index Desired starting position (optional).
+		* 
+		* @return Index the string is found at or -1 if not found.
+		*/
+		int Find(std::string s, unsigned int index = 0) const;
+
+		// FOR TESTING
 		std::string GetString(unsigned int pos);
 	};
 }
--- Week 1/Lab01/src/Array_List.cpp
+++ Week 1/Lab01/src/Array_List.cpp
@@ -1,4 +1,6 @@
 #include <Array_List.h>
+#include <iostream>
+#include <stdexcept>
 
 ssuds::ArrayList::ArrayList() {
 	_data = nullptr;
@@ -14,7 +16,7 @@ void ssuds::ArrayList::Append(std::string s) {
 	if (_data == nullptr && _size == 0) {
 		// === New list ===
 		// Set the size of the array list to hold one data item
-		_size = 1;
+		_size++;
 
 		// Allocate memory for the array of strings
 		_data = new std::string[_size];
@@ -24,7 +26,7 @@ void ssuds::ArrayList::Append(std::string s) {
 	else {
 		// === List already made ===
 		// Increase array size
-		_size += 1;
+		_size++;
 
 		// Allocate a temporary array
 		std::string* tempData = new std::string[_size];
@@ -43,13 +45,114 @@ void ssuds::ArrayList::Append(std::string s) {
 		// Set the '_data' pointer to the 'tempData' pointer
 		_data = tempData;
 	}
+
+	return;
 }
 
-unsigned int ssuds::ArrayList::Size() {
+unsigned int ssuds::ArrayList::Size() const {
 	return _size;
 }
 
+void ssuds::ArrayList::Insert(std::string s, const unsigned int index) {
+	// Check that the desired index is in range
+	if (index >= 0 && index < _size + 1) {
+		// Check that the string is capable of insertion
+		//	1. If array list is empty
+		//	2. If the position will be the last position of the array
+		if ((_data == nullptr && _size == 0) || index == (_size + 1)) {
+			// Append value
+			Append(s);
 
+			return;
+		}
+		// Increase array size
+		_size++;
+
+		// Allocate a temporary array
+		std::string* tempData = new std::string[_size];
+
+		// Check that 'index' is greater than 0
+		if (index != 0) {
+			// Loop through the array
+			for (int i = 0; i < _size; i++) {
+				// Copy data from before the insertion index
+				if (i < index) {
+					tempData[i] = _data[i];
+				}
+				// Insertion
+				else if (i == index) {
+					tempData[index] = s;
+				}
+				// Copy data from after the insertion index
+				else if (i > index) {
+					tempData[i] = _data[i - 1];
+				}
+			}
+		}
+		else {
+			// Insertion
+			tempData[0] = s;
+
+			// Copy remainder of data
+			for (int i = 1; i < _size; i++) {
+				tempData[i] = _data[i - 1];
+			}
+		}
+
+		// Free up old array
+		delete[] _data;
+
+		// Assign 'tempData' pointer to '_data'
+		_data = tempData;
+	}
+	else {
+		throw std::out_of_range("ERROR: 'INDEX' is NOT in range of the list!\n");
+	}
+
+	
+
+	return;
+}
+
+void ssuds::ArrayList::Clear() {
+	// Free up allocated memory
+	delete[] _data;
+
+	// Set the list variables to default values
+	_size = 0;
+	_data = nullptr;
+}
+
+std::string ssuds::ArrayList::Get(const unsigned int index) const {
+	// Check that 'index' is in range
+	if (index >= 0 && index < _size) {
+		// Return the string at the index position of the list
+		return _data[index];
+	}
+	else {
+		throw std::out_of_range("ERROR: 'INDEX' is NOT in range of the list!\n");
+	}
+}
+
+int ssuds::ArrayList::Find(std::string s, unsigned int index) const {
+	// Check that 'index' is in range
+	if (index >= 0 && index < _size) {
+		// Loop through the list
+		for (int i = 0; i < _size; i++) {
+			// Check if the string has been found
+			if (_data[i] == s) {
+				// Return index
+				return i;
+			}
+		}
+
+		// Index was not found, return -1
+		return -1;
+	}
+	else {
+		throw std::out_of_range("ERROR: 'INDEX' is NOT in range of the list!\n");
+	}
+}
 
 // Using to test
 std::string ssuds::ArrayList::GetString(unsigned int pos) {
