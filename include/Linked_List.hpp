2478c18e04db958f9cce98b63fecc8b64447f04b
Parker Lauders <parkerlauders04@gmail.com>
2024-02-18 19:00:26 -0500

    Added iterator functionality

--- include/Linked_List.hpp
+++ include/Linked_List.hpp
@@ -33,10 +33,10 @@ namespace ssuds {
 			/***************************************************************************//**
 			* @brief Override constructor of internal _Node class.
 			*
-			* Initializes defaule values with data being set to a provided value.
+			* Initializes default values with data being set to a provided value.
 			******************************************************************************/
 			_Node(const L& data) {
-				// Set default values
+				// Set default with provided values
 				_data = data;
 				_next = _prev = nullptr;
 			}
@@ -51,6 +51,116 @@ namespace ssuds {
 		// Variable to hold the size of the linked list
 		unsigned int _size;
 	public:
+		class LinkedListIterator {
+		protected:
+			// Pointer to an LinkedList node
+			_Node* _ptr;
+
+			// Iterator index
+			unsigned int _index;
+		public:
+			/***************************************************************************//**
+			* @brief Constructor of internal LinkedListIterator class.
+			*
+			* Initializes default values for internal LinkedListIterator class.
+			******************************************************************************/
+			LinkedListIterator() {
+				// Set default values
+				_ptr = nullptr;
+				_index = 0;
+			}
+
+			/***************************************************************************//**
+			* @brief Override constructor of LinkedListIterator class.
+			*
+			* Initializes default values when data is provided.
+			******************************************************************************/
+			LinkedListIterator(_Node* node, unsigned int index) {
+				// Set default with provided values
+				_ptr = node;
+				_index = index;
+			}
+
+			/***************************************************************************//**
+			* @brief Dereference operator.
+			*
+			* @return A reference to the "current" value.
+			******************************************************************************/
+			L& operator*() const {
+				return _ptr->_data;
+			}
+
+			/***************************************************************************//**
+			* @brief Increment operator .
+			*
+			* Advance iterator to the next value.
+			* 
+			* @return Iterator.
+			******************************************************************************/
+			LinkedListIterator& operator++() {
+				// Get the node after
+				_ptr = _ptr->_next;
+
+				// Increase index
+				++_index;
+
+				return *this;
+			}
+
+			/***************************************************************************//**
+			* @brief Decrement operator.
+			*
+			* Decrease iterator to the previous value.
+			* 
+			* @return Iterator.
+			******************************************************************************/
+			LinkedListIterator& operator--() {
+				// Get the node before
+				_ptr = _ptr->_prev;
+				
+				// Decrease index
+				--_index;
+
+				return *this;
+			}
+
+			/***************************************************************************//**
+			* @brief Equal comparison operator.
+			*
+			* Test if two iterators are equal.
+			* 
+			* @return Return true if this iterator is iterating through the same LinkedList
+			* and is at the same spot as 'other'.
+			******************************************************************************/
+			bool operator==(const LinkedListIterator& other) const {
+				// Return 'TRUE' if they are alike 'other', false if aren't
+				return _index == other._index && _ptr == other._ptr;
+			}
+
+			/***************************************************************************//**
+			* @brief Nonequal comparison operator
+			*
+			* Test if two iterators are not equal.
+			* 
+			* @return Return true if this iterator is iterating through a different LinkedList
+			* and is at a different spot as 'other'.
+			******************************************************************************/
+			bool operator!=(const LinkedListIterator& other) const {
+				// Return 'TRUE' if they aren't alike 'other', false if are
+				return !(*this == other);
+			}
+
+			/***************************************************************************//**
+			* @brief Function to get the index.
+			*
+			* Gets the index at the current moment.
+			*
+			* @return The index.
+			******************************************************************************/
+			unsigned int Index() {
+				return _index;
+			}
+		};
 		/***************************************************************************//**
 		* @brief Constructor of LinkedList class.
 		*
@@ -156,7 +266,7 @@ namespace ssuds {
 		/***************************************************************************//**
 		* @brief Append a item to the end of the LinkedList.
 		*
-		* @param s The item to be appended.
+		* @param item The item to be appended.
 		******************************************************************************/
 		void Append(const L& item) {
 			// Create a new node
@@ -186,7 +296,7 @@ namespace ssuds {
 		/***************************************************************************//**
 		* @brief Prepend an item to the beignning of the LinkedList.
 		*
-		* @param s The item to be prepended.
+		* @param item The item to be prepended.
 		******************************************************************************/
 		void Prepend(const L& item) {
 			// Create a new node
@@ -211,6 +321,82 @@ namespace ssuds {
 
 			// Increment the list size
 			_size++;
+		}
+
+		/***************************************************************************//**
+		* @brief Insert an item at a index in the LinkedList.
+		*
+		* @param index The position to be inserted.
+		* 
+		* @param item The item to be inserted.
+		******************************************************************************/
+		void Insert(unsigned int index, const L& item) {
+			// Check 'index' for valid operation
+			if (index == 0) {
+				Prepend(item);
+			}
+			else if (index == _size) {
+				Append(item);
+			}
+			else if (index >= 0 && index < _size) {
+				// Variable to hold current node
+				_Node* current;
+
+				// Create a new node
+				_Node* nnode = new _Node(item);
+
+				// See if it would be better the start from the beginning or end
+				if (index <= ((_size - 1) / 2)) {
+					// Set current to the start address
+					current = _start;
+
+					// Loop to 'index'
+					for (unsigned int i = 0; i < index - 1; i++) {
+						// Get the next node
+						current = current->_next;
+					}
+
+					// Set the new node previous to the current node
+					nnode->_prev = current;
+
+					// Set the new nodes next to that of current nodes
+					nnode->_next = current->_next;
+
+					// Set the node following current to point back to new node
+					current->_next->_prev = nnode;
+
+					// Set the current next to new node
+					current->_next = nnode;
+				}
+				else {
+					// Set current to the end address
+					current = _end;
+
+					// Loop to 'index'
+					for (unsigned int i = (_size - 1); i > index; i--) {
+						// Get the next node
+						current = current->_prev;
+					}
+
+					// Set the new node nex to current node
+					nnode->_next = current;
+
+					// Set the new nodes prev to that of current nodes
+					nnode->_prev = current->_prev;
+
+					// Set the node before current to point forward to the new node
+					current->_prev->_next = nnode;
+
+					// Set the current prev to new node
+					current->_prev = nnode;
+				}
+
+				// Increase size
+				_size++;
+			}
+			else {
+				throw std::out_of_range("Index is out of range!");
+			}
 		}
 
 		/***************************************************************************//**
@@ -246,6 +432,71 @@ namespace ssuds {
 			// Reset the other LinkedList variables to default
 			_start = _end = nullptr;
 			_size = 0;
+		}
+
+		/***************************************************************************//**
+		* @brief Used to get the beginning of the iterator.
+		* 
+		* @return The iterator pointing to the beginning of the LinkedList.
+		******************************************************************************/
+		LinkedListIterator begin() const {
+			LinkedListIterator iter(_start, 0);
+			return iter;
+		}
+
+		/***************************************************************************//**
+		* @brief Used to get the ending of the iterator.
+		* 
+		* @return The iterator pointing to one slot past the end of the LinkedList
+		******************************************************************************/
+		LinkedListIterator end() const {
+			LinkedListIterator iter(nullptr, _size);
+			return iter;
+		}
+
+		/***************************************************************************//**
+		* @brief Used to get the beginning of the reverse iterator.
+		*
+		* @return The iterator pointing to one slot past the end of the LinkedList
+		******************************************************************************/
+		LinkedListIterator rbegin() const {
+			LinkedListIterator iter(_end, _size - 1);
+			return iter;
+		}
+
+		/***************************************************************************//**
+		* @brief Used to get the ending of the reverse iterator.
+		*
+		* @return The iterator pointing to one slot past the end of the LinkedList
+		******************************************************************************/
+		LinkedListIterator rend() const {
+			LinkedListIterator iter(nullptr, -1);
+			return iter;
+		}
+
+		/***************************************************************************//**
+		* @brief Used to find an element in the LinkedList.
+		* 
+		* @param item The item to be located.
+		*
+		* @return The iterator pointing to the element.
+		******************************************************************************/
+		LinkedListIterator Find(const L& item) const {
+			// Start from the beginning of the LinkedList
+			LinkedListIterator it = begin();
+
+			// Iterate through the list
+			while (it != end()) {
+				// Check the current element
+				if (*it == item) {
+					return it;
+				}
+
+				// Advance the iterator
+				++it;
+			}
+
+			return end();
 		}
 	};
 }
