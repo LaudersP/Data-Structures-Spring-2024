ffef44ae1e84ecba84d59b63cb003533ae32fb13
Parker Lauders <parkerlauders04@gmail.com>
2024-03-12 20:27:28 -0400

    Added function code

--- include/Queue.hpp
+++ include/Queue.hpp
@@ -6,22 +6,39 @@
 namespace ssuds {
 	template <class T>
 	class Queue : private LinkedList<T> {
-		// No Attributes
+	public:
+		// Iterator 
+		using Iterator = typename LinkedList<T>::LinkedListIterator;
 
+		// Adds element to the end of queue
 		void enqueue(const T& new_val) {
 			this->Append(new_val);
 		}
 
-		void dequeue(const T& val) {
-			// Remove queue item
+		// Removes element from the beginning of queue
+		void dequeue() {
+			// Check if the queue is empty
+			if (Empty()) 
+				throw std::out_of_range("ERROR) Queue is empty, cannont remove front element!");
+
+			this->Remove(this->begin());
 		}
 
-		void Top() {
-			// Get the top element, without removing it
+		// Function to get the top element without removing it
+		T& Top() const {
+			// Check if the queue is empty
+			if (Empty()) 
+				throw std::out_of_range("ERROR) Queue is empty, cannont access front element!");
+
+			return _start->_data;
 		}
 
-		void Empty() {
-			// Return true if empty
+		// Function to check if stack is empty
+		void Empty() const {
+			if (this->Size() == 0)
+				return true;
+			else
+				return false;
 		}
 	};
 }
