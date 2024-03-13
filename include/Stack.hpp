4d42281ac51ef77c78bb45bebe6e30ade413c23e
Parker Lauders <parkerlauders04@gmail.com>
2024-03-12 22:21:58 -0400

    Added Size and Clear functions

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -1,5 +1,5 @@
-#ifndef STACK_H
-#define STACK_H
+#ifndef STACK_HPP
+#define STACK_HPP
 
 #include <Linked_List.hpp>
 
@@ -14,11 +14,15 @@ namespace ssuds {
 		// Iterator 
 		using Iterator = typename LinkedList<T>::LinkedListIterator;
 	public:
-		typename LinkedList<T>::LinkedListIterator begin() {
+		~Stack() {
+			Clear();
+		}
+
+		Iterator begin() {
 			return _list.begin();
 		}
 
-		typename LinkedList<T>::LinkedListIterator end() {
+		Iterator end() {
 			return _list.end();
 		}
 
@@ -31,20 +35,35 @@ namespace ssuds {
 
 		// Function to remove an item from the top of stack
 		void Pop() {
-			_list.remove(_list.begin());
+			if (Empty())
+				throw std::out_of_range("ERROR: Stack is empty, cannot remove front element!");
+
+			_list.Remove(_list.begin());
 		}
 
 		// Function to get the top element without removing it
-		T& Top() {
-			// Get the top element, without removing it
+		T& Top() const {
+			if (Empty())
+				throw std::out_of_range("ERROR: Stack is empty, cannot access front element!");
+
+			return *(_list.begin());
 		}
 
 		// Function to check if stack is empty
-		void Empty() {
-			if (_list.Size() == 0)
-				return true;
-			else
-				return false;
+		bool Empty() const {
+			return _list.Size() == 0 ? true : false;
+		}
+
+		// Function to return stack size 
+		unsigned int Size() const {
+			return _list.Size();
+		}
+
+		// Function to remove all stack items
+		void Clear() {
+			while (!Empty()) {
+				Pop();
+			}
 		}
 	};
 }
