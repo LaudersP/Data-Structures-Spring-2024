a2ec0ae798f9d7a2de340d5b5d542a32966c5fc8
Parker Lauders <parkerlauders04@gmail.com>
2024-03-14 15:05:45 -0400

    Added function to keep the queue full

--- include/WordDrawer.hpp
+++ include/WordDrawer.hpp
@@ -1,20 +1,25 @@
 #ifndef WORD_DRAWER_HPP
 #define WORD_DRAWER_HPP
 
-#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics.hpp>
+#include <TextCircle.hpp>
 #include <string>
 #include <fstream>
+#include <Queue.hpp>
 
 class WordDrawer {
 // === Attributes ===
 private:
 	std::ifstream _SCOWL;
 	sf::Font _font;
+	ssuds::Queue<std::string> _wordQueue;
+	unsigned int _queueSize = 10;
 
 // === Functions ===
 public:
 	WordDrawer(const std::string& wordListPath, const sf::Font& font);
 	~WordDrawer();
+	void FillQueue();
 };
 
 #endif
--- src/WordDrawer.cpp
+++ src/WordDrawer.cpp
@@ -1,4 +1,11 @@
+// References
+// Mouse events: 
+// https://en.sfml-dev.org/forums/index.php?topic=15275.0
+// https://www.sfml-dev.org/tutorials/2.6/window-events.php
+
 #include <WordDrawer.hpp>
+
+#include <iostream>
 
 WordDrawer::WordDrawer(const std::string& wordListPath, const sf::Font& font) : _font(font) {
 	// Open word list
@@ -11,4 +18,13 @@ WordDrawer::~WordDrawer() {
 	if (_SCOWL.is_open())
 		_SCOWL.close();
 }
-// Code here...
+
+// Function to ensure there is always 10 words in the queue
+void WordDrawer::FillQueue() {
+	while (_wordQueue.Size() < _queueSize) {
+		std::string line;
+
+		if (std::getline(_SCOWL, line))
+			_wordQueue.enqueue(line);
+	}
+}
