2d0dda72e6b3dc49c8868f71539ffbe652e74157
Parker Lauders <parkerlauders04@gmail.com>
2024-03-18 15:42:35 -0400

    Added GoogleTest code for new functions

--- src/OrderedSet_Tests.cpp
+++ src/OrderedSet_Tests.cpp
@@ -4,6 +4,39 @@
 #include <gtest/gtest.h>
 #include <OrderedSet.hpp>
 
-// Google test code here...
+class OrderedSetTestFixtureINT : public testing::Test {
+protected:
+	void SetUp() override {
+		tree.insert(175);
+		tree.insert(26);
+		tree.insert(8);
+		tree.insert(67);
+		tree.insert(400);
+		tree.insert(852);
+	}
+
+	// Make an instance
+	ssuds::OrderedSet<int> tree;
+};
+
+TEST_F(OrderedSetTestFixtureINT, SizeTest) {
+	ssuds::OrderedSet<int> tree2;
+	EXPECT_EQ(tree2.size(), 0);
+
+	EXPECT_EQ(tree.size(), 6);
+}
+
+TEST_F(OrderedSetTestFixtureINT, InsertTest) {
+	EXPECT_TRUE(tree.insert(867));
+	EXPECT_TRUE(tree.insert(637));
+	EXPECT_TRUE(tree.insert(794));
+	EXPECT_TRUE(tree.insert(768));
+
+	EXPECT_FALSE(tree.insert(768));
+	EXPECT_FALSE(tree.insert(175));
+	EXPECT_FALSE(tree.insert(8));
+
+	EXPECT_EQ(tree.size(), 10);
+}
 
 #endif
