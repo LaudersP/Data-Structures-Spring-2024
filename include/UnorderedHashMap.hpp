a23d315fe261f5babe9ac72bf5dd124f71494049
Parker Lauders <parkerlauders04@gmail.com>
2024-03-25 14:05:26 -0400

    Initial Commit

---
+++ include/UnorderedHashMap.hpp
@@ -0,0 +1,13 @@
+#ifndef UNORDERED_HASH_MAP
+#define UNORDERED_HASH_MAP
+
+#include <utility>
+
+namespace ssud {
+	template <class K, class V>
+	class UnorderedMap {
+		//  Code here...
+	};
+}
+
+#endif
