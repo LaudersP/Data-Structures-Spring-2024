d0ca5a643d9db2730c29364b2b6d37c84bc2ad6e
Parker Lauders <parkerlauders04@gmail.com>
2024-04-08 19:38:33 -0400

    Bug fix

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -5,18 +5,40 @@
 
 namespace ssuds {
 	template <class N, class E>
+	/**
+	* @brief A template class for creating and managing a graph data structure.
+    * 
+    * This class uses nested unordered maps to represent a graph, where each node
+    * can be connected to other nodes with edges that have associated values.
+	*/
 	class Graph {
 	protected:
-		// Unordered hash map to store a map of node edges
+		/**
+		* @brief Stores the graph data.
+		*/
 		ssuds::UnorderedMap<N, ssuds::UnorderedMap<N, E>> _data;
 
 	public:
-		// Method to add a new node
+		/**
+		* @brief Adds a new node to the graph.
+		*
+		* If the node already exists, this function does nothing.
+		*
+		* @param val The value of the node to be added.
+		*/
 		void add_node(const N& val) {
 			_data[val];
 		}
 
-		// Method to add a connection between nodes
+		/**
+		* @brief Adds an edge between two existing nodes.
+		*
+		* If either of the nodes does not exist, throws std::out_of_range.
+		*
+		* @param start_node The starting node of the edge.
+		* @param dest_node The destination node of the edge.
+		* @param val The value of the edge.
+		*/
 		void add_edge(const N& start_node, const N& dest_node, const E& val) {
 			// Check that 'start_node' and 'dest_node' exist
 			if (!_data.contains(start_node) || !_data.contains(dest_node))
@@ -26,12 +48,25 @@ namespace ssuds {
 			return;
 		}
 		
-		// Method to check if a node exists
+		/**
+		* @brief Checks if a node exists in the graph.
+		*
+		* @param node The node to check.
+		* @return True if the node exists, false otherwise.
+		*/
 		bool contains_node(const N& node) const {
 			return _data.contains(node);
 		}
 
-		// Method to check if a edge exists
+		/**
+		* @brief Checks if an edge exists between two nodes.
+		*
+		* If either of the nodes does not exist, throws std::out_of_range.
+		*
+		* @param start_node The starting node of the edge.
+		* @param end_node The ending node of the edge.
+		* @return True if the edge exists, false otherwise.
+		*/
 		bool contains_edge(const N& start_node, const N& end_node) const {
 			// Check that 'start_node' and 'dest_node' exist
 			if (!_data.contains(start_node) || !_data.contains(end_node))
@@ -48,7 +83,13 @@ namespace ssuds {
 			return false;
 		}
 
-		// Method to remove a node
+		/**
+		* @brief Removes a node and all edges connected to it.
+		*
+		* If the node does not exist, throws std::out_of_range.
+		*
+		* @param node The node to remove.
+		*/
 		void remove_node(const N& node) {
 			// Check that 'node' exists
 			if(!_data.contains(node))
@@ -67,7 +108,14 @@ namespace ssuds {
 				_data.remove(node_iter);
 		}
 
-		// Method to remove a edge
+		/**
+		* @brief Removes an edge between two nodes.
+		*
+		* If either node does not exist, or if the edge does not exist, throws std::out_of_range.
+		*
+		* @param start_node The start node of the edge to remove.
+		* @param end_node The end node of the edge to remove.
+		*/
 		void remove_edge(const N& start_node, const N& end_node) {
 			// Check that 'start_node' and 'dest_node' exist
 			if (!_data.contains(start_node) || !_data.contains(end_node))
@@ -86,7 +134,15 @@ namespace ssuds {
 				throw std::out_of_range("ERROR: Edge does not exist!\n");
 		}
 
-		// Method to get a edge
+		/**
+		* @brief Gets the value of an edge between two nodes.
+		*
+		* If either node does not exist, or if the edge does not exist, throws std::out_of_range.
+		*
+		* @param start_node The start node of the edge.
+		* @param dest_node The destination node of the edge.
+		* @return The value of the edge.
+		*/
 		E get_edge(const N& start_node, const N& dest_node) {
 			// Check that 'start_node' and 'dest_node' exist
 			if (!_data.contains(start_node) || !_data.contains(dest_node))
@@ -99,6 +155,15 @@ namespace ssuds {
 				throw std::out_of_range("ERROR: Edge does not exist!\n");
 		}
 
+		/**
+		* @brief Overloads the output stream operator to print the graph's contents.
+		*
+		* Iterates through each node and its edges, printing them in a readable format.
+		*
+		* @param os The output stream.
+		* @param graph The graph to print.
+		* @return The output stream.
+		*/
 		friend std::ostream& operator<<(std::ostream& os, const Graph& graph) {
 			// Iterate through the nodes
 			for (auto node_iter = graph._data.begin(); node_iter != graph._data.end(); ++node_iter) {
